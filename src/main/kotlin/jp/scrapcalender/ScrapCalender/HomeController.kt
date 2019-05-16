dd706099107fdc27be8f190cc5be62fec8d1e99d
takatsuki-takumi <takatsuki.takumi@moneyforward.co.jp>
2019-05-16 19:06:10 +0900

    add:scraper

--- src/main/kotlin/jp/scrapcalender/ScrapCalender/HomeController.kt
+++ src/main/kotlin/jp/scrapcalender/ScrapCalender/HomeController.kt
@@ -1,12 +1,9 @@
 package jp.scrapcalender.ScrapCalender
 
-import org.hibernate.validator.constraints.URL
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.sql.Connection
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.insert
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.ui.Model
@@ -15,10 +12,9 @@ import org.springframework.web.servlet.ModelAndView
 import org.jsoup.Jsoup
 import java.lang.Exception
 import java.security.MessageDigest
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
+import java.util.Date
+import kotlin.collections.ArrayList
 
 
 //hash関数
@@ -32,21 +28,28 @@ private fun hashString(input: String): String{
 }
 
 //define table
+object URL_DATALINK: Table(){
+    val url = text("url").primaryKey()
+    val data_link = text("data_link").uniqueIndex()
+}
 object URL_TIME_SPAN:Table(){
     var url = text("url").primaryKey()
     var date = text("date")
     var span = integer("span")
 }
-
-//view画面処理
-@Controller
-class ViewController {
-    @GetMapping("/view")
-    fun view(): String {
-        println(sha256("rilakkuma"))
-        return "view"
-    }
+object URLHASH_TYPE_SELECTER_ID:Table(){
+    var urlhash = text("urlhash")
+    var type = text("type")
+    var selecter = text("selecter")
+    var id = integer("id").autoIncrement()
 }
+object URLHASH_DATE_DATA_ID:Table(){
+    var urlhash = text("urlhash")
+    var date = text("date")
+    var data = text("data")
+    var id = integer("id").autoIncrement()
+}
+
 
 //select画面処理
 @Controller
@@ -117,6 +120,7 @@ class Controller {
         return rtn
     }
 
+    //confirm画面処理
     @GetMapping("/confirm")
     fun confirm(@RequestParam geturl: String,@RequestParam(defaultValue = "") error: String,model:Model): String{
         if (error != ""){
@@ -129,6 +133,7 @@ class Controller {
         return "confirm"
     }
 
+    //spanタイムエラー処理
     @GetMapping("/check_time")
     fun check_time(@RequestParam geturl: String, @RequestParam time_span: String, model: Model): String{
         var rtn = "redirect:complete?geturl=" + geturl + "&time_span=" + time_span
@@ -150,6 +155,7 @@ class Controller {
         return rtn
     }
 
+    //コンプリート画面処理
     @GetMapping("/complete")
     fun complete(@RequestParam geturl: String, @RequestParam time_span: String, model: Model): String{
         Database.connect("jdbc:sqlite:./SCDB.sqlite3", "org.sqlite.JDBC")
@@ -157,19 +163,44 @@ class Controller {
             val cal: Calendar = Calendar.getInstance (TimeZone.getDefault(), Locale.getDefault())
             cal.add(Calendar.MINUTE, time_span.toInt())
             var date_add:Date = cal.getTime()
-            /*URL_TIME_SPAN.insert {
+            URL_TIME_SPAN.insert {
                 it[url] = geturl
                 it[date] = date_add.toString()
                 it[span] = time_span.toInt()
-            }*/
+            }
             var url_hash = sha256(geturl)
             var view_link = "./view?view_link=" + url_hash
             URL_DATALINK.insert {
                 it[url] = geturl
                 it[data_link] = view_link
             }
+            for(list in search_list){
+                URLHASH_TYPE_SELECTER_ID.insert{
+                    it[urlhash] = url_hash
+                    it[type] = list[0]
+                    it[selecter] = list[1]
+                }
+            }
         }
         return "complete"
+    }
+
+    //view画面処理
+    @GetMapping("/view")
+    fun view(@RequestParam view_link:String, model: Model,mav: ModelAndView): ModelAndView {
+        mav.setViewName("view")
+        var listinlist:ArrayList<ArrayList<String>> = arrayListOf()
+        Database.connect("jdbc:sqlite:./SCDB.sqlite3", "org.sqlite.JDBC")
+        transaction(transactionIsolation = Connection.TRANSACTION_SERIALIZABLE, repetitionAttempts = 1) {
+            for(column in URLHASH_DATE_DATA_ID.select(URLHASH_DATE_DATA_ID.urlhash eq view_link).orderBy(URLHASH_DATE_DATA_ID.date,isAsc = false)){
+                var templist:ArrayList<String> = arrayListOf()
+                templist.add(column[URLHASH_DATE_DATA_ID.date].toString())
+                templist.add(column[URLHASH_DATE_DATA_ID.data].toString())
+                listinlist.add((templist))
+            }
+            mav.addObject("data_list",listinlist)
+        }
+        return mav
     }
 
 }
--- src/main/kotlin/jp/scrapcalender/ScrapCalender/scraper.kt
+++ src/main/kotlin/jp/scrapcalender/ScrapCalender/scraper.kt
@@ -1,32 +1,24 @@
 package jp.scrapcalender.ScrapCalender
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
 import org.jetbrains.exposed.sql.*
-import org.jsoup.Jsoup
-import java.sql.Connection
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import java.sql.Connection
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 import java.util.Date
-import javax.print.Doc
 
-@SpringBootApplication
-class ScrapCalenderApplication
-
-//main function
-fun main(args: Array<String>) {
+fun main(){
     //var url = get_latest_url()
-    //scrape(url)
-	runApplication<ScrapCalenderApplication>(*args)
+    //scrap(url)
 }
-
+/*
 fun get_latest_url(): String{
     Database.connect("jdbc:sqlite:./SCDB.sqlite3", "org.sqlite.JDBC")
     var latest_url = ""
@@ -35,25 +27,41 @@ fun get_latest_url(): String{
             latest_url = column[URL_TIME_SPAN.url].toString()
         }
     }
+    println(latest_url)
     return latest_url
 }
+*/
 
-fun scrape(url: String) {
-    Database.connect("jdbc:sqlite:./SCDB.sqlite3", "org.sqlite.JDBC")
+/*
+fun scrap(url: String) {
     val document: Document = Jsoup.connect(url).get()
     var url_hash = sha256(url)
-    var cal: Calendar = Calendar.getInstance (TimeZone.getDefault(), Locale.getDefault())
+    val cal: Calendar = Calendar.getInstance (TimeZone.getDefault(), Locale.getDefault())
     var date_now: Date = cal.getTime()
-    transaction (transactionIsolation = Connection.TRANSACTION_SERIALIZABLE, repetitionAttempts = 1){
-        for (column in URLHASH_TYPE_SELECTER_ID.select(URLHASH_TYPE_SELECTER_ID.urlhash eq url_hash)){
+    transaction {
+        for (column in URLHASH_TYPE_SELECTER_ID.select(URLHASH_TYPE_SELECTER_ID.id eq url_hash)){
             var elems: Elements = document.select(column[URLHASH_TYPE_SELECTER_ID.selecter].toString())
             for(elem: Element in elems){
                 URLHASH_DATE_DATA_ID.insert {
                     it[urlhash] = url_hash
                     it[date] = date_now.toString()
-                    it[data] = elem.text()
+                    it[data] = elem.toString()
                 }
             }
         }
     }
 }
+*/
+
+
+/*
+var str = "Thu May 16 09:54:10 JST 2019"
+//str to date
+var format = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
+var todate = format.parse(str)
+//date to cal
+val cal_int: Calendar = Calendar.getInstance ()
+cal_int.setTime(todate)
+//cal to date
+var cal_to_date:Date = cal_int.getTime()
+ */
